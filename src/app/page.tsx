7e9753b103b2712cae32e9a4c8270201a697a21e
syafikadin <syafik.adin222@gmail.com>
2024-11-27 16:22:18 +0700

    Finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
 "use client";
 
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 export default function Home() {
-
   const [isMusic, setIsMusic] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const [currentImage, setCurrentImage] = useState(0);
+  const [sliderImage, setSliderImage] = useState(0);
   const audioRef = useRef<HTMLAudioElement | null>(null);
 
   const images = [
     "https://invitato.net/test-product-engineer/static/2-9fafa4bf7091b5207804ffe51f518939.jpg",
     "https://invitato.net/test-product-engineer/static/5-ffa38a07e15195800fbcc590cb50b2d0.jpg",
-    "https://invitato.net/test-product-engineer/static/1-2b43ea516254cdff99c88a7fce90ae98.jpg"
+    "https://invitato.net/test-product-engineer/static/1-2b43ea516254cdff99c88a7fce90ae98.jpg",
   ];
 
   const handlePlayAudio = () => {
@@ -35,11 +35,30 @@ export default function Home() {
   };
 
   const goToNextImage = () => {
-    setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+    setSliderImage((prevImage) => (prevImage + 1) % images.length);
   };
 
   const goToPreviousImage = () => {
-    setCurrentImage((prevImage) => (prevImage - 1 + images.length) % images.length);
+    setSliderImage((prevImage) => (prevImage - 1 + images.length) % images.length);
+  };
+
+  const handlePreviewClick = (index: number) => {
+    setSliderImage(index);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prev) => (prev + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const scrollToText = () => {
+    const element = document.getElementById("scroll-text");
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
   };
 
   return (
@@ -66,12 +85,14 @@ export default function Home() {
           </p>
         </div>
         {isOpen && (
-          <button
-            className="btn btn-sm btn-light position-absolute bottom-0 start-0 m-3"
-            onClick={handlePauseAudio}
-          >
-            Music
-          </button>
+          <>
+            <button
+              className="btn btn-sm btn-light position-absolute bottom-0 start-0 m-3"
+              onClick={handlePauseAudio}
+            >
+              <i className="bi bi-music-note"></i>
+            </button>
+          </>
         )}
       </div>
 
@@ -79,10 +100,12 @@ export default function Home() {
       {isOpen ? (
         <div className="col-12 col-lg-5 position-relative overflow-auto" style={{ maxHeight: "100vh" }}>
           <img
-            src="/img/img1.jpg"
-            alt="Couple Picnic"
+            src={images[currentImage]}
+            alt="Wedding Gallery"
             className="img-fluid vh-100 w-100 object-fit-cover"
-            style={{ objectPosition: "center" }}
+            style={{
+              opacity: 1,
+            }}
           />
           <div className="position-absolute top-0 start-0 w-100 h-100 bg-dark bg-opacity-50 text-white d-flex flex-column">
             <div className="text-center">
@@ -92,40 +115,105 @@ export default function Home() {
               <h2 className="text-uppercase wedding-title-2">Tiffany & Jared</h2>
               <h2 className="fst-italic timetoshare">#TImetoshaRE</h2>
             </div>
+
+            <div className="position-absolute bottom-0 end-0 m-4 text-center text-white">
+              <button
+                className="btn btn-transparent text-white border-0 button-scroll"
+                onClick={scrollToText}
+                style={{
+                  backgroundColor: 'transparent',
+                  border: 'none',
+                }}
+              >
+                SCROLL TO BEGIN <i className="bi bi-arrow-down"></i>
+              </button>
+            </div>
+
           </div>
 
-          <div className="d-flex flex-column mt-5 text-center">
-            <p className="dear-text">DEAR MR-MRS-MS,</p>
-            <p className="dear-text">FAMILY & FRIENDS</p>
-            <h2 className="welcome-text mt-2">Welcome to <br /> Tiffany & Jared's <br /> Wedding Website</h2>
-            <p className="caption-text mt-3">Together with joyful hearts and the grace of God, we joyfully announce the upcoming of our marriage.</p>
-          </div>
+          <div className="d-flex flex-column">
+            <div className="text-center mt-5" id="scroll-text">
+              <p className="dear-text">DEAR MR-MRS-MS,</p>
+              <p className="dear-text">FAMILY & FRIENDS</p>
+              <h2 className="welcome-text mt-2">
+                Welcome to <br /> Tiffany & Jared's <br /> Wedding Website
+              </h2>
+              <p className="caption-text mt-3">
+                Together with joyful hearts and the grace of God, we joyfully announce the
+                upcoming of our marriage.
+              </p>
+            </div>
 
-          {/* Gambar yang bisa digeser */}
-          <div className="d-flex justify-content-center mt-4">
-            <img
-              src={images[currentImage]}
-              alt="Wedding Gallery"
-              className="img-fluid"
-              style={{ maxHeight: "300px", objectFit: "cover" }}
-            />
-          </div>
+            <div className="d-flex align-items-center justify-content-center position-relative mt-4">
+              {/* Preview Kiri */}
+              <div
+                className="position-absolute d-flex align-items-center"
+                style={{
+                  left: "30%",
+                  zIndex: 1,
+                  cursor: "pointer",
+                  transform: "translateX(-120%)",
+                }}
+                onClick={() => handlePreviewClick((sliderImage - 1 + images.length) % images.length)}
+              >
+                <img
+                  src={images[(sliderImage - 1 + images.length) % images.length]}
+                  alt="Previous"
+                  className="img-thumbnail"
+                  style={{
+                    maxHeight: "380px",
+                    objectFit: "cover",
+                  }}
+                />
+              </div>
+              {/* Gambar Utama */}
+              <img
+                src={images[sliderImage]}
+                alt="Wedding Gallery"
+                className="img-fluid"
+                style={{
+                  maxHeight: "400px",
+                  objectFit: "cover",
+                  border: "2px solid white",
+                }}
+              />
+              {/* Preview Kanan */}
+              <div
+                className="position-absolute d-flex align-items-center"
+                style={{
+                  right: "30%",
+                  zIndex: 1,
+                  cursor: "pointer",
+                  transform: "translateX(120%)",
+                  overflow: 'hidden'
+                }}
+                onClick={() => handlePreviewClick((sliderImage + 1) % images.length)}
+              >
+                <img
+                  src={images[(sliderImage + 1) % images.length]}
+                  alt="Next"
+                  className="img-thumbnail"
+                  style={{
+                    maxHeight: "380px",
+                    objectFit: "cover",
+                  }}
+                />
+              </div>
+            </div>
 
-          {/* Tombol untuk menggeser gambar */}
-          <div className="d-flex justify-content-end me-5 mt-4">
-            <button
-              className="btn btn-light me-2 border btn-arrow"
-              onClick={goToPreviousImage}>
-              <i className="bi bi-arrow-left fs-4"></i>
-            </button>
-            <button
-              className="btn btn-light me-5 border btn-arrow"
-              onClick={goToNextImage}>
-              <i className="bi bi-arrow-right fs-4"></i>
-            </button>
+            {/* Button Left Right */}
+            <div className="d-flex justify-content-end me-5 mt-4">
+              <button className="btn btn-light me-2 border btn-arrow" onClick={goToPreviousImage}>
+                <i className="bi bi-arrow-left fs-4"></i>
+              </button>
+              <button className="btn btn-light me-5 border btn-arrow" onClick={goToNextImage}>
+                <i className="bi bi-arrow-right fs-4"></i>
+              </button>
+            </div>
           </div>
         </div>
       ) : (
+        // Image Before Open
         <div className="col-12 col-lg-5 position-relative">
           <img
             src="https://ik.imagekit.io/drpq5xrph/Template%20Tiffany%20&%20Jared/1.%20Cover.jpg?updatedAt=1698222296920"
@@ -140,8 +228,12 @@ export default function Home() {
             <div className="d-flex flex-column justify-content-center align-items-center h-100">
               <h2 className="text-uppercase wedding-title-2">Tiffany & Jared</h2>
               <h2 className="fst-italic timetoshare">#TImetoshaRE</h2>
-              <button className="btn btn-light mt-2 px-5 py-1 btn-up-down"
-                onClick={handlePlayAudio}>Open</button>
+              <button
+                className="btn btn-light mt-2 px-5 py-1 btn-up-down"
+                onClick={handlePlayAudio}
+              >
+                Open
+              </button>
             </div>
           </div>
         </div>
